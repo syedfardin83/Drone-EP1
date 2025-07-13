f4f75c4655fbf13500310f80a80ba998077f3bbf
Syed Fardin <syedfardin0612@gmail.com>
2025-07-13 22:40:52 +0530

    Lark1 Flight computer power input schematic kicad done

---
+++ ESP32/raw_4motors_test/raw_4motors_test.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(23,OUTPUT);
+  pinMode(22,OUTPUT);
+  pinMode(21,OUTPUT);
+  pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(23,HIGH);
+  digitalWrite(22,HIGH);
+  digitalWrite(21,HIGH);
+  digitalWrite(13,HIGH);
+}
